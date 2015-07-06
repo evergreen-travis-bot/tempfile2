95e5e71274804d6d1bf4de1857afdd656ec2b493
Kiko Beats <josefrancisco.verdu@gmail.com>
2015-07-06 09:26:27 +0100

    added default value for ensure param

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 'use strict';
 
-var nodePath = require('path');
-var uuid     = require('uuid');
-var mkdirp   = require('mkdirp');
-var TMP_DIR  = require('os-tmpdir')();
+var nodePath      = require('path');
+var uuid          = require('uuid');
+var mkdirp        = require('mkdirp');
+var TMP_DIR       = require('os-tmpdir')();
+var existsDefault = require('existential-default');
 
 module.exports = function(params) {
 
   // backward compatibility
-  if (typeof params === 'string')
-    return nodePath.join(TMP_DIR, uuid.v4() + (params || ''));
+  if (typeof params === 'string') return nodePath.join(TMP_DIR, uuid.v4() + (params || ''));
 
   if (!params) params = {};
+  var ext = existsDefault(params.ext, '');
+  var path = existsDefault(params.path, TMP_DIR);
+  var ensure = existsDefault(params.ensure, true);
 
-  var ext = params.ext || '';
-  var path = params.path || TMP_DIR;
-
-  if (params.ensure) mkdirp.sync(path, {mode: params.mode});
+  if (ensure) mkdirp.sync(path, {mode: params.mode});
   return nodePath.join(path, uuid.v4() + ext);
 };
