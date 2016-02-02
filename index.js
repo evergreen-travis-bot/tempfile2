5e0769130d6c308d7a3a47c4c486cbc9b5ab0a30
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-02-02 10:21:58 +0100

    Update scaffold

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-'use strict';
+'use strict'
 
-var nodePath      = require('path');
-var uuid          = require('uuid');
-var mkdirp        = require('mkdirp');
-var TMP_DIR       = require('os-tmpdir')();
-var existsDefault = require('existential-default');
+var nodePath = require('path')
+var uuid = require('uuid')
+var mkdirp = require('mkdirp')
+var TMP_DIR = require('os-tmpdir')()
+var existsDefault = require('existential-default')
 
-module.exports = function(params) {
-
+module.exports = function (params) {
   // backward compatibility
-  if (typeof params === 'string') return nodePath.join(TMP_DIR, uuid.v4() + (params || ''));
+  if (typeof params === 'string') return nodePath.join(TMP_DIR, uuid.v4() + (params || ''))
 
-  if (!params) params = {};
+  if (!params) params = {}
 
-  var ext = existsDefault(params.ext, '');
-  var path = existsDefault(params.path, TMP_DIR);
-  var ensure = existsDefault(params.ensure, true);
-  var filename = existsDefault(params.name, uuid.v4());
+  var ext = existsDefault(params.ext, '')
+  var path = existsDefault(params.path, TMP_DIR)
+  var ensure = existsDefault(params.ensure, true)
+  var filename = existsDefault(params.name, uuid.v4())
 
-  if (ensure) mkdirp.sync(path, {mode: params.mode});
-  return nodePath.join(path, filename + ext);
-};
+  if (ensure) mkdirp.sync(path, {mode: params.mode})
+  return nodePath.join(path, filename + ext)
+}
