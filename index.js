cebd8bda5e31cd336b9179084ac930e35b02e575
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-06-14 15:28:15 +0200

    Fix typo

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function tempfile2 (opts) {
   // backward compatibility
   if (typeof opts === 'string') return path.join(TMP_DIR, uuid.v4() + (opts || ''))
   opts = Object.assign({}, DEFAULTS(), opts)
-  return path.join(opts.path, opts.filename + opts.ext)
+  return path.join(opts.path, opts.name + opts.ext)
 }
 
 module.exports = tempfile2
